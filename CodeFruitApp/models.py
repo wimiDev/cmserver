942d603d4c9cfc5b7092d204c2b2835aef1eefa4
zengqingwang <wimidev@foxmail.com>
2018-07-09 20:50:51 +0800

    完整登录，注册逻辑

--- CodeFruitApp/models.py
+++ CodeFruitApp/models.py
@@ -3,3 +3,29 @@ class User(models.Model):
 
     UserName = models.CharField(max_length=20)
     UserPass = models.CharField(max_length=20)
+
+    ONLINE = 'ONLINE'
+    OFFLINE = 'OFFLINE'
+    LOGIN_STATE = (
+        (ONLINE, 'online'),
+        (OFFLINE, 'offline'),
+    )
+    #logon state
+    LoginState = models.CharField(
+                     max_length=20,
+                     choices = LOGIN_STATE,
+                     default=OFFLINE,
+    )
+
+    NORMAL = 'NORMAL'
+    FROZEN = 'FROZEN'
+    ACCOUNT_STATE = (
+    	(NORMAL,'normal'),
+    	(FROZEN,'frozen'),
+    )
+    #account state
+    AccountState = models.CharField(
+    	max_length=20,
+    	choices = ACCOUNT_STATE,
+    	default = NORMAL,
+    )
--- CodeFruitApp/views.py
+++ CodeFruitApp/views.py
@@ -9,15 +9,50 @@ class UserRegister(APIView):
         get = request.GET
         username = get.get('username')
         password = get.get('password')
-
-        User.objects.get(UserName=username)
-        User.objects.create(UserName="12344", UserPass="21312")
-
-        # 返回信息
+        
+        users = User.objects.filter(UserName=username)
         data = {
             'status': 1,
-            'message': 'success',
-            'username': username,
-            'password': password,
-        }
+            'message': 'account exist',
+            }
+        if len(users) <= 0:
+           User.objects.create(UserName=username, UserPass=password)
+           # 返回信息
+           data = {
+             'status':0,
+             'message':'register success',
+             'username':username,
+             }
         return JsonResponse(data)
+
+class UserLogin(APIView):
+    def get(self,request):
+        get = request.GET
+        username = get.get('username')
+        password = get.get('password')
+        
+        users = User.objects.filter(UserName=username)
+        data = {
+            'status': 1,
+            'message': 'account not exist',
+            }
+        if len(users) > 0:
+            user = users[0]
+            if(user.UserPass == password):
+                data = {
+                    'status': 0,
+                    'message': 'login sucess',
+                    'username':username
+                }
+
+            else:
+           # 返回信息
+                data = {
+                     'status':2,
+                     'message':'account or passworld error',
+                }
+            
+
+        return JsonResponse(data)
+    
+
